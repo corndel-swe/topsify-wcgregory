451b702c7b8606427db30ebe84cb011130cde59a
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-11 09:03:07 +0000

    removing SQL query

--- exercises/6b.sql
+++ exercises/6b.sql
@@ -2,14 +2,3 @@
 -- Include the total duration of the album
 -- Sort the results by album duration, with the longest first
 
-SELECT
-    albums.name AS album_name,
-    SUM(tracks.duration_ms) AS total_duration
-FROM
-    albums
-JOIN
-    tracks ON albums.id = tracks.album_id
-GROUP BY
-    albums.id
-ORDER BY
-    total_duration DESC;
