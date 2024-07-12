d6f85116b06425a3daac8800644221e980434ce4
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-12 13:22:53 +0000

    completed exercises up to 6a

--- exercises/5a.sql
+++ exercises/5a.sql
@@ -1,1 +1,4 @@
 -- How many tracks are longer than 5 minutes?
+SELECT COUNT(tracks.id)
+FROM tracks
+WHERE tracks.duration_ms > 300000;
--- exercises/6a.sql
+++ exercises/6a.sql
@@ -2,3 +2,8 @@
 -- Include a count of how many albums they have released
 -- Order your results by artist name in ascending order
 -- (You should group the artists by id, not by name)
+SELECT artists.name, COUNT(albums.id)
+FROM albums
+JOIN artists on albums.artist_id = artists.id
+GROUP BY artists.id
+ORDER BY artists.name;
