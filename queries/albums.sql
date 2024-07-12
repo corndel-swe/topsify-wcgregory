745ca228fc0124e6f00a91bfb3de6c38e9bc78ca
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-12 14:35:12 +0000

    amended album page to include artists name instead of artists id

--- queries/albums.sql
+++ queries/albums.sql
@@ -2,8 +2,8 @@
 -- retrieve the name, artist_id, release_date and image_url
 -- sqlite3 db/db.sqlite < queries/albums.sql > reports/albums.json
 .mode json
-SELECT albums.name, albums.artist_id, albums.release_date, albums.image_url
+SELECT albums.name, artists.name AS artist_name, albums.release_date, albums.image_url
 FROM albums
-ORDER BY release_date DESC
+INNER JOIN artists ON albums.artist_id = artists.id
+ORDER BY release_date DESC;
 -- LIMIT 20 OFFSET 0;
--- INNER JOIN artists ON albums.artist_id = artists.id
