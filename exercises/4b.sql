a8b95c9848119759960ba544f5a7843d856d4c5f
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-12 10:56:29 +0000

    completed day2 - exercise 4

--- exercises/4b.sql
+++ exercises/4b.sql
@@ -1,3 +1,8 @@
 -- List the 10 slowest tracks in the database
 -- Order by tempo, with the slowest first
 -- Include the track id, track title and track tempo
+SELECT tracks.id, tracks.name, features.tempo
+FROM tracks
+INNER JOIN features ON tracks.id = features.track_id
+ORDER BY features.tempo
+LIMIT 10;
