7a023a86d889bfc60e6b60706d16093b124a0b0b
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-15 09:14:42 +0000

    chart3 completed - removed unused sql statements

--- queries/chart-3.sql
+++ queries/chart-3.sql
@@ -5,9 +5,7 @@ SELECT
     AVG(features.loudness) AS average_loudness,
     albums.release_date
 FROM albums
--- INNER JOIN artists ON albums.artist_id = artists.id
 INNER JOIN tracks ON albums.id = tracks.album_id
 INNER JOIN features ON tracks.id = features.track_id
 GROUP BY albums.id
 ORDER BY average_loudness
--- ORDER BY average_danceability DESC;
